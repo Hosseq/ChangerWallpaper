d10cf8846e515eac4ce6d5cc4c609576d59177b3
Istkliff <garnakot191@gmail.com>
2020-03-20 14:17:57 +0300

    Final Update
    - Application installs itself in StartUp
    - New icon

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
 using Microsoft.Win32;
 using System.Runtime.InteropServices;
 
@@ -10,15 +8,13 @@ namespace ChangerWallpaper
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			while(true)
-			{
-				Console.WriteLine("What wallpaper you need? (a,b,c)");
-				string wallpaper = Console.ReadLine();
-				ChangeWallpaper("T:/Projects/ChangerWallpaper/Pictures/" + wallpaper + ".jpg", 0, 0);
-			}
-		}
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		const int SW_HIDE = 0;
 
 		const int SPI_SETDESKWALLPAPER = 20;
 		const int SPIF_UPDATEINIFILE = 0x01;
@@ -28,13 +24,43 @@ namespace ChangerWallpaper
 		static extern int SystemParametersInfo
 			(int uAction, int uParam, string lpvParam, int fuWinIni);
 
-		public static void ChangeWallpaper(string path, int style, int tile)
+		static void Main(string[] args)
+		{
+			SystemEvents.SessionEnding += OnSessionEnding;
+
+			var handle = GetConsoleWindow();
+			ShowWindow(handle, SW_HIDE);
+
+			CheckStartUp();
+
+			Console.Read();
+		}
+
+		static void CheckStartUp()
+		{
+			if(File.Exists(@"C:\Users\ReJ\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ChangerWallpaper.exe") == false)
+			{
+				File.Copy(Directory.GetCurrentDirectory() + @"\ChangerWallpaper.exe", @"C:\Users\ReJ\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ChangerWallpaper.exe");
+			}
+		}
+
+		static void ChangeWallpaper(string path, int style, int tile)
 		{
 			RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
 			key.SetValue("WallpaperStyle", style.ToString());
 			key.SetValue("TileWallpaper", tile.ToString());
 			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path,
 			SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+			key.Close();
+		}
+
+		static void OnSessionEnding(object sender, SessionEndingEventArgs args)
+		{
+			if (args.Reason == SessionEndReasons.SystemShutdown)
+			{
+				Random rnd = new Random();
+				ChangeWallpaper("T:/Projects/ChangerWallpaper/Pictures/" + rnd.Next(1,10) + ".jpg", 0, 0);
+			}
 		}
 	}
 }
